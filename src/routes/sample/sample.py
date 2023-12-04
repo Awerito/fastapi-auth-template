747e36e94416b93af4d0fc95e74e8b02561cf415
Awerito <diego.i.munoz.viveros@gmail.com>
2023-12-04 12:43:41 -0300

    Add: basic CRUD class for endpoint abstraction

--- src/routes/sample/sample.py
+++ src/routes/sample/sample.py
@@ -1,13 +1,39 @@
-from fastapi import APIRouter, HTTPException, Depends, Security, status
+from bson import ObjectId
+from fastapi import APIRouter
 
-from src.auth import User, current_active_user
-
-
-sample_router = APIRouter()
+from src.orm.crud import CRUD
 
 
-@sample_router.get("/", tags=["Sample"])
-async def sample(
-    current_user: User = Security(current_active_user, scopes=["sample.read"])
-):
-    return {"message": "Hello World"}
+sample_router = APIRouter(tags=["Sample"], prefix="/sample")
+sample_collection = CRUD("sample")
+
+
+@sample_router.get("/")
+async def get_all_sample(skip: int = 0, limit: int = 100):
+    """Get all samples"""
+    return sample_collection.read_all({}, skip, limit)
+
+
+@sample_router.get("/{sample_id}")
+async def get_sample(sample_id: str):
+    """Get sample by id"""
+    sample = sample_collection.read({"_id": sample_id})
+    return sample
+
+
+@sample_router.post("/")
+async def create_sample(sample: dict):
+    """Create sample"""
+    return sample_collection.create(sample)
+
+
+@sample_router.put("/{sample_id}")
+async def update_sample(sample: dict, sample_id: str):
+    """Update sample"""
+    return sample_collection.update({"_id": sample_id}, sample)
+
+
+@sample_router.delete("/{sample_id}")
+async def delete_sample(sample_id: str):
+    """Delete sample"""
+    return sample_collection.delete({"_id": sample_id})
