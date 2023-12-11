c02ee4a4609c0b41dc60928571f627fe720bda15
Awerito <diego.i.munoz.viveros@gmail.com>
2023-12-11 11:53:26 -0300

    Add: update relations on cascade

--- src/orm/crud.py
+++ src/orm/crud.py
@@ -7,9 +7,10 @@ from src.database import db
 
 
 class CRUD:
-    def __init__(self, collection):
+    def __init__(self, collection, relations=[]):
         self.collection = collection
         self.db = db[collection]
+        self.relations = relations
 
     def create(self, data):
         data["disable"] = False
@@ -55,8 +56,16 @@ class CRUD:
             if ObjectId.is_valid(query["_id"]):
                 query["_id"] = ObjectId(query["_id"])
 
-        results = self.db.update_one(query, {"$set": data})
-        return bool(results.modified_count)
+        results = self.db.find_one_and_update(
+            query, {"$set": data}, return_document=True
+        )
+        if bool(results):
+            for relation in self.relations:
+                update_status = db[relation].update_many(
+                    {self.collection: results["_id"]}, {"$set": results}
+                )
+                print(update_status)
+        return bool(results)
 
     def delete(self, query):
         if "_id" in query:
